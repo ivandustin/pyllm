6d1f2860fec4fe68cce74be999adffb7cb9b3684
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-05-09 13:11:33 +0800

    Clean up

--- pyllm/openai/chat.py
+++ pyllm/openai/chat.py
@@ -6,8 +6,6 @@ logger = logging.getLogger(__name__)
 
 
 def chat(prompt, **kwargs):
-    logger.info(f"Input\n{prompt}")
-    logger.info(f"Parameters\n{kwargs}")
     response = openai.ChatCompletion.create(
         messages=[
             {
@@ -17,7 +15,6 @@ def chat(prompt, **kwargs):
         ],
         **kwargs,
     )
+    logger.info(f"Parameters\n{kwargs}")
     logger.info(f"Response\n{response}")
-    output = choice(response["choices"])["message"]["content"]
-    logger.info(f"Output\n{output}")
-    return output
+    return choice(response["choices"])["message"]["content"]
