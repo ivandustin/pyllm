518681ec4482d6d913655b0af5f9ef5cab2afcc4
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-05-09 13:35:29 +0800

    Add completion

--- pyllm/openai/__init__.py
+++ pyllm/openai/__init__.py
@@ -1,4 +1,7 @@
 from functools import partial
+from .completion import completion
 from .chat import chat
 
 llm = partial(chat, model="gpt-3.5-turbo", temperature=0)
+
+__all__ = ["llm", "completion", "chat"]
